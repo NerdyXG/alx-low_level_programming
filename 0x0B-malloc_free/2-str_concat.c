86bdeeacfcfe6c1d8deb7bef24600c73afb10061
NerdyXG <MichaelOluwayemi11@gmail.com>
2023-03-14 03:28:03 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,10 @@ char *str_concat(char *s1, char *s2)
 
 	s = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s == NULL)
+	{
+		free(s);
 		return (NULL);
+	}
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
